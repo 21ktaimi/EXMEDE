50ca38c20f8cec768b30d8dec23c9733ef2df098
profeRSG <116580578+profeRSG@users.noreply.github.com>
2022-12-07 19:23:52 +0100

    fix para el examen
    fix para el examen

--- ProyectoEjemploGithub/src/Main.java
+++ ProyectoEjemploGithub/src/Main.java
@@ -3,9 +3,22 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello");
+		contar(5,10);//Queremos pintar un cuadrado de tamaño 5
 
+	}
 
+/**
+ * El siguiente metodo muestra los numeros que hay entre el parametro desde y hasta,
+ * separados por comas,  por ejemplo 2,3,4
+ * 
+ * @param desde indica el numero por el que se empieza a contar
+ * @param hasta indica el numero hasta el que se quiere contar
+ */
+	private static void contar(int desde, int hasta) {
+		for(int i=desde;i<=hasta-1;i++) {
+			System.out.print(i+",");
+		}
+		System.out.print(hasta);
 	}
 
 }
